59d272ddc1e24329a9b8fa39b2884e98bc793424
3ae3ae <ogyshjsm@naver.com>
2023-04-11 17:22:21 +0900

    하는 방법 메뉴 만드는 중 애니메이션 효과 추가 메인화면 만드는 중

--- setting.js
+++ setting.js
@@ -1,2 +1,33 @@
-const wrap = document.getElementById("wrap");
-const cS = window.getComputedStyle(wrap);
+function showManual() {
+  const $blur = document.getElementById("blur");
+  $blur.classList.toggle("none");
+  const blurAnimation = (s) => ($blur.style.backdropFilter = `blur(${s}px)`);
+  linearAnimation(blurAnimation, 0, 5, 0.3, 60);
+  const $manual = document.getElementById("manual");
+  $manual.classList.toggle("none");
+  $manual.classList.toggle("flex");
+  const invisibleAnimation = (s) => ($manual.style.opacity = s);
+  linearAnimation(invisibleAnimation, 0, 1, 0.3, 60);
+  $manual.addEventListener("click", hideManual);
+}
+
+function hideManual() {
+  const $manual = document.getElementById("manual");
+  const invisibleAnimation = (s) => ($manual.style.opacity = s);
+  linearAnimation(invisibleAnimation, 1, -1, 0.3, 60);
+  const $blur = document.getElementById("blur");
+  const blurAnimation = (s) => ($blur.style.backdropFilter = `blur(${s}px)`);
+  linearAnimation(blurAnimation, 5, -5, 0.3, 60);
+  setTimeout($manual.classList.toggle, 300, "none");
+  setTimeout($manual.classList.toggle, 300, "flex");
+  setTimeout($blur.classList.toggle, 300, "none");
+}
+
+function linearAnimation(animation, init, strength, time, frame) {
+  for (let i = 0; i <= time; i += 1 / frame) {
+    setTimeout(animation, i * 1000, init + (i * strength) / time);
+  }
+}
+
+const $how_to = document.getElementById("how_to");
+$how_to.addEventListener("click", showManual);
